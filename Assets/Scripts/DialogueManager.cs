3c3fead3754722a9750ceb3ee231bb12c4aa17a6
Thessa Vu <tlv220000@utdallas.edu>
2023-07-30 18:43:17 -0500

    Added function onto DialogueManager Script
    Added DisplayNextSentence() and EndDialogue()

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -11,9 +11,33 @@ public class DialogueManager : MonoBehaviour
         _sentences = new Queue<string>();
     }
 
-    public void StartDialogue (Dialogue dialogue)
+    public void StartDialogue(Dialogue dialogue)
     {
         Debug.Log("Starting conversation with " + dialogue.name);
+
+        _sentences.Clear();
+
+        foreach (string sentence in dialogue._sentences)
+        {
+            _sentences.Enqueue(sentence);
+        }
     }
+        public void DisplayNextSentence()
+        {
+            if(_sentences.Count == 0)
+            {
+                EndDialogue();
+                return;
+            }
+
+            string sentence = _sentences.Dequeue();
+            Debug.Log(sentence);
+        }
+
+        void EndDialogue()
+        {
+            Debug.Log("End of conversation.");
+        }
+    
    
 }
